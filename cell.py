a3321a0021b1ff8c97716c305e900aaf1e4a21ea
Sean Bain <artemis45566@gmail.com>
2023-06-30 12:07:30 +0800

    Finished cell draw method.

--- cell.py
+++ cell.py
@@ -1,17 +1,46 @@
+from line import Line
+from point import Point
+
+
 class Cell:
 
-    def __init__(self, win, point_1, point_2):
+    def __init__(self, win):
 
-        self.__win = win
-        self.__x1 = None
-        self.__y1 = None
-        self.__x2 = None
-        self.__y2 = None
-        has_left_wall = True
-        has_right_wall = True
-        has_top_wall = True
-        has_bottom_wall = True
+        self.win = win
+        self.canvas = win.canvas
+        self.x1 = None
+        self.y1 = None
+        self.x2 = None
+        self.y2 = None
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
 
 
-    def draw(self):
+    def draw(self, point_1, point_2):
+        self.x1 = point_1.x
+        self.y1 = point_1.y
+        self.x2 = point_2.x
+        self.y2 = point_2.y
+
+        point_3 = Point(self.x2, self.y1)
+        point_4 = Point(self.x1, self.y2)
+
+        if self.has_left_wall:
+            left_wall = Line(point_1, point_4)
+            left_wall.draw(self.canvas, 'red')
+
+        if self.has_right_wall:
+            right_wall = Line(point_3, point_2)
+            right_wall.draw(self.canvas, 'red')
+
+        if self.has_top_wall:
+            right_wall = Line(point_1, point_3)
+            right_wall.draw(self.canvas, 'red')
+
+        if self.has_bottom_wall:
+            right_wall = Line(point_4, point_2)
+            right_wall.draw(self.canvas, 'red')
+
 
--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from tkinter import Tk, BOTH, Canvas
 from line import Line
 from point import Point
+from cell import Cell
 class Window:
 
     def __init__(self, width, height):
 
-        self.__root = Tk()
-        self.__root.title = 'Maze Solver'
-        self.__canvas = Canvas(self.__root)
-        self.__canvas.pack()
+        self.root = Tk()
+        self.root.title = 'Maze Solver'
+        self.canvas = Canvas(self.root, height=height, width=width)
+        self.canvas.pack()
         self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self.root.update_idletasks()
+        self.root.update()
 
     def wait_for_close(self):
         self.running = True
@@ -25,16 +26,15 @@ class Window:
         self.running = False
 
     def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
+        line.draw(self.canvas, fill_color)
 
 
 def main():
     win = Window(800, 600)
-    point_1 = Point(20, 50)
-    point_2 = Point(40, 80)
-    line_1 = Line(point_1, point_2)
-    win.draw_line(line_1, 'red')
-
+    point_1 = Point(20, 40)
+    point_2 = Point(60, 80)
+    cell = Cell(win)
+    cell.draw(point_1, point_2)
 
     win.wait_for_close()
 
